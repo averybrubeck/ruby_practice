ccbf31d7797e42f40a62a635eab70a73c4a09e5d
averybrubeck <averybrubeck@yahoo.com>
2023-09-10 12:13:29 -0400

    ruby practice

---
+++ case.rb
@@ -0,0 +1,11 @@
+a = 5
+a = gets.chomp.to_i
+answer = case a
+when 5
+     "a is 5"
+when 6
+     "a is 6"
+else  
+     "a is neither 5 or 6"
+end
+puts answer
---
+++ conditionals.rb
@@ -0,0 +1,11 @@
+puts "put in a number"
+number = gets.chomp.to_i
+
+if number == 3
+    puts "a is 3"
+    
+elsif number == 4
+    puts "a is 4"
+else 
+    puts "a is not 3 or 4"
+end

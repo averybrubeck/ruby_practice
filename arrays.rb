4bf2a2f6e14b6adc333bec7f09315bc75dea953b
averybrubeck <averybrubeck@yahoo.com>
2023-09-13 20:57:21 -0400

    more practice

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,11 @@
+#this is a ruby comment
 arr = [1, 3, 5, 7, 9, 11]
-number = 3
-if arr.select{|a| a == number}
+number = gets.chomp
+if arr.include?number
     puts "the number is in the array"
 else
     puts "the number is not in the array"
 end
+
+
 
